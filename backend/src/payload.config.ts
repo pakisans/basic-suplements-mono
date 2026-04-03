2a17f789f2a1905eff82472b0040cd89a2397e14
pakisans <pakisakac@gmail.com>
2026-04-04 01:02:14 +0200

    cors

--- backend/src/payload.config.ts
+++ backend/src/payload.config.ts
@@ -33,10 +33,7 @@ const dirname = path.dirname(filename)
 export default buildConfig({
   serverURL: process.env.PAYLOAD_PUBLIC_SERVER_URL || 'http://localhost:3001',
   cors: [
-    'http://localhost:3000',
-    'http://localhost:3001',
     'http://46.225.222.58',
-    'http://46.225.222.58:3001',
     // process.env.PAYLOAD_PUBLIC_SERVER_URL,
     // process.env.NEXT_PUBLIC_SERVER_URL,
   ].filter(Boolean) as string[],
