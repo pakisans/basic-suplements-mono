631159d620d59cf990e92161893df014bee363f6
pakisans <pakisakac@gmail.com>
2026-04-03 20:38:23 +0200

    push

--- backend/src/payload.config.ts
+++ backend/src/payload.config.ts
@@ -31,14 +31,15 @@ const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
 
 export default buildConfig({
-  // cors: [
-  //   'http://localhost:3000',
-  //   'http://localhost:3001',
-  //   'http://46.225.222.58',
-  //   'http://46.225.222.58:3001',
-  //   process.env.PAYLOAD_PUBLIC_SERVER_URL,
-  //   process.env.NEXT_PUBLIC_SERVER_URL,
-  // ].filter(Boolean) as string[],
+  serverURL: process.env.PAYLOAD_PUBLIC_SERVER_URL || 'http://localhost:3001',
+  cors: [
+    'http://localhost:3000',
+    'http://localhost:3001',
+    'http://46.225.222.58',
+    'http://46.225.222.58:3001',
+    // process.env.PAYLOAD_PUBLIC_SERVER_URL,
+    // process.env.NEXT_PUBLIC_SERVER_URL,
+  ].filter(Boolean) as string[],
   // csrf: [
   //   'http://localhost:3000',
   //   'http://localhost:3001',
